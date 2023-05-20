f3ddb734bd75231d8a335d949219f578ef732bc7
Jahir Zakaria <jahir.zakaria.work@gmail.com>
2023-05-20 02:24:45 +0200

    config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: PUT, GET, POST, OPTIONS, DELETE');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Headers: Content-Type');
 
 $host = "db";
 $db_name = "ecommerce";
